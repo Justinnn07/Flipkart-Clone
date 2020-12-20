6f5c815982a0c4fa53c95374f9811481da8dafb7
Justinnn07 <justinv2006@gmail.com>
2020-12-20 20:36:54 +0530

    initial build in progress

---
+++ src/Header.js
@@ -0,0 +1,148 @@
+import { Button, Input, makeStyles, Modal } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { auth, provider } from "./firebase";
+import "./Header.css";
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const Header = () => {
+  const googlee = (e) => {
+    e.preventDefault();
+    auth.signInWithPopup(provider).catch((err) => alert(err.message));
+  };
+  const signup = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => alert(err.message));
+  };
+  const login = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) =>
+        alert(
+          err.message,
+          "PLEASE MAKE A NEW ACCOUNT OR CHECK YOUR EMAIL OR PASSWORD!"
+        )
+      );
+  };
+  useEffect(() => {
+    auth.onAuthStateChanged((auth) => {
+      console.log(auth);
+    });
+  });
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <center>
+        <img
+          src="https://logos-download.com/wp-content/uploads/2016/09/Flipkart_logo.png"
+          alt=""
+          className="img-img"
+        />
+        <div className="iinput">
+          <Input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="btns">
+            <Button onClick={login}>Login</Button>
+            <Button onClick={signup}>SignUp</Button>
+          </div>
+          <div className="google">
+            <i class="fab fa-google" onClick={googlee}></i>
+          </div>
+        </div>
+      </center>
+    </div>
+  );
+  return (
+    <div className="header">
+      <form className="form">
+        <center>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            {body}
+          </Modal>
+        </center>
+      </form>
+      <div className="header-input">
+        <img
+          src="https://img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+          alt=""
+          className="header-logo"
+        />
+        <div className="header-center">
+          <input
+            type="text"
+            className="input"
+            placeholder="Search for products, brands and more "
+          />
+          <div class="button">
+            <i class="fas fa-search"></i>
+          </div>
+        </div>
+      </div>
+      <div className="header-options">
+        <button className="btn" onClick={handleOpen}>
+          Login
+        </button>
+        {/* <SimpleModal /> */}
+        <a href="https://justinnn07.netlify.app/">Home</a>
+        <a href="/">More</a>
+        <a href="/">
+          <i class="fas fa-shopping-cart"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDYd0PB8Jgm9jndFp4KcvsJSJ4ia-j-EQE",
+  authDomain: "flipkart-clone1.firebaseapp.com",
+  projectId: "flipkart-clone1",
+  storageBucket: "flipkart-clone1.appspot.com",
+  messagingSenderId: "834525107235",
+  appId: "1:834525107235:web:2e51625097588757cfcd96",
+  measurementId: "G-1GL4P564VH",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebaseApp.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
